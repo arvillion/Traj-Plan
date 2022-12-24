e51b11316dad4860c473de6308234c758a06b668
arv <arv@example.com>
2022-12-24 14:17:55 +0800

    update

--- ev2.py
+++ ev2.py
@@ -8,7 +8,7 @@ import torch
 from itertools import product
 import numpy as np
 
-N_EVALS = 400
+N_EVALS = 100
 
 def param_gen(param_dict: dict):
     items = param_dict.items()
@@ -20,7 +20,7 @@ def param_gen(param_dict: dict):
 
 if __name__ == "__main__":
     n_targets = 40
-    agent = Agent()
+
 
     # This is a example of what the evaluation procedure looks like.
     # The whole dataset is divided into a training set and a test set.
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     data = torch.load("data.pth")
     label = data["label"]
     feature = data["feature"]
-
+    agent = Agent()
 
     param_dict = {
         "stdx": np.arange(1., 4., .1, dtype=np.float32),
@@ -56,35 +56,36 @@ if __name__ == "__main__":
 
     manager = Manager()
     best_result = manager.dict({ "score": -math.inf, "param": None })
-    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
-        futures = [executor.submit(evap, param, best_result) for param in param_gen(param_dict)]
-        for future in concurrent.futures.as_completed(futures):
-            param, score = future.result()
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+    #     futures = [executor.submit(evap, param, best_result) for param in param_gen(param_dict)]
+    #     for future in concurrent.futures.as_completed(futures):
+    #         param, score = future.result()
+    #
+    #         if score > best_result["score"]:
+    #             best_result["score"] = score
+    #             best_result["param"] = param
+    #
+    #         print(f"Score: {score}")
+    #         print(f"Param: {param}")
+    #         print(f"Best score: {best_result['score']}")
+    #         print(f"Best param: {best_result['param']}")
+    #         print()
+    # print(dict)
 
-            if score > best_result["score"]:
-                best_result["score"] = score
-                best_result["param"] = param
 
-            print(f"Score: {score}")
-            print(f"Param: {param}")
-            print(f"Best score: {best_result['score']}")
-            print(f"Best param: {best_result['param']}")
-            print()
-    print(dict)
-
-
-    # for param in tqdm(param_gen(param_dict)):
-    #     scores = []
-    #     for _ in range(N_EVALS):
-    #         target_pos, target_features, target_cls, class_scores = generate_game(n_targets, N_CTPS, feature, label)
-    #         ctps_inter = agent.get_action(target_pos, target_features, class_scores, **param)
-    #         score = evaluate(compute_traj(ctps_inter), target_pos, class_scores[target_cls], RADIUS)
-    #         scores.append(score)
-    #     mean_score = torch.stack(scores).float().mean()
-    #     if mean_score > best_score:
-    #         best_score = mean_score
-    #         best_param = param
-    #     print(f"Score: {mean_score}")
-    #     print(f"Param: {param}")
-    #     print(f"Best score: {best_score}")
-    #     print(f"Best param: {best_param}")
+    for param in tqdm(param_gen(param_dict)):
+        scores = []
+        for _ in range(N_EVALS):
+            target_pos, target_features, target_cls, class_scores = generate_game(n_targets, N_CTPS, feature, label)
+            ctps_inter = agent.get_action(target_pos, target_features, class_scores, **param)
+            score = evaluate(compute_traj(ctps_inter), target_pos, class_scores[target_cls], RADIUS)
+            scores.append(score)
+        mean_score = torch.stack(scores).float().mean()
+        if score > best_result["score"]:
+            best_result["score"] = score
+            best_result["param"] = param
+        print(f"Score: {score}")
+        print(f"Param: {param}")
+        print(f"Best score: {best_result['score']}")
+        print(f"Best param: {best_result['param']}")
+        print()
